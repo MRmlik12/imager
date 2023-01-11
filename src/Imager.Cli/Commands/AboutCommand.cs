fa06678e348978ac68fb24a17d5f0b2a56028009
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2023-01-11 21:57:38 +0100

    Add about command (#6)

---
+++ src/Imager.Cli/Commands/AboutCommand.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Imager.Cli.Commands;
+
+public class AboutCommand : Command
+{
+    public override int Execute(CommandContext context)
+    {
+        var imagerVersion = typeof(AboutCommand).Assembly.GetName().Version;
+
+        AnsiConsole.MarkupLine("[yellow]Author: [link=https://github.com/MRmlik12]Daniel Olczyk (MRmlik12)[/][/]");
+        AnsiConsole.MarkupLine("[aqua]Repository: [link]https://github.com/MRmlik12/imager[/][/]");
+        AnsiConsole.MarkupLine($"[purple].NET Version: {Environment.Version}[/]");
+        AnsiConsole.MarkupLine($"[blue]imager version: {imagerVersion}[/]");
+
+        return 0;
+    }
+}
