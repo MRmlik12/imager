406c8d187609d218a30c4bbb96a34388d576d8a0
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2023-01-16 17:59:04 +0100

    Add chocolatey support (#12)
    * Add chocolatey support
    
    * Add more tags

---
+++ deploy/chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unzipLocation = $toolsDir + "\bin"
+
+$packageArgs = @{
+  packageName   = 'imager'
+  unzipLocation = $unzipLocation
+  file          = Get-Item $toolsDir\*.zip
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item $toolsDir\*.zip
