fab7a2abb2db3e0e957d10ff553dd9e1ca99fc19
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2023-02-08 22:36:06 +0100

    Add rotate command (#15)

---
+++ src/Imager.Backend/Operations/RotateImage.cs
@@ -0,0 +1,17 @@
+﻿using NetVips;
+
+namespace Imager.Backend.Operations;
+
+public static class RotateImage
+{
+    public static void Rotate(string sourcePath, string outputPath, double degrees)
+    {
+        using var image = Image.NewFromFile(sourcePath);
+        using var rotatedImage = image.Rotate(degrees);
+        
+        rotatedImage.WriteToFile(outputPath, new VOption
+        {
+            { "Q", 100 }
+        });
+    }
+}
---
+++ src/Imager.Cli/Commands/RotateCommand.cs
@@ -0,0 +1,23 @@
+﻿using Imager.Backend.Exceptions;
+using Imager.Backend.Operations;
+using Imager.Backend.Validate;
+using Imager.Cli.Options;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Imager.Cli.Commands;
+
+public class RotateCommand : Command<RotateOptions>
+{
+    public override int Execute(CommandContext context, RotateOptions settings)
+    {
+        if (!PathValidator.ValidatePath(settings.SourcePath))
+            throw new ItemNotExistsException();
+        
+        AnsiConsole.MarkupLine($"Rotating an image...");
+        RotateImage.Rotate(settings.SourcePath, settings.OutputPath, settings.Degrees);
+        AnsiConsole.MarkupLine("[underline green]Done[/]  :check_mark_button:");
+
+        return 0;
+    }
+}
